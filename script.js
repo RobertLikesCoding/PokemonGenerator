a55b183b2b4d2a15c51f986bd058efa12087e777
RobertLikesCoding <rbaufeld@posteo.de>
2024-01-09 13:01:33 +0100

    fixed the math.floor functions
    I deleted the ' + 1' after Math.random because indexes always start at 0.

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const elementArray = ["fire","water","grass","electric","psychic","metal","dark"
 const hpRange = Math.floor(Math.random()* 7) * 10 + 60;
 
 const namesPicker = () => {
-    let randomDigit = Math.floor(Math.random()* 24);
-    return namesArray[randomDigit];
+    let randomDigit = Math.floor(Math.random()* 23);
+    if (namesArray[randomDigit].length < 6) {
+        return namesArray[randomDigit] + "mon";
+    } else {
+        return namesArray[randomDigit];
+    }
 }
 
 const elementPicker = () => {
-    let randomDigit = Math.floor(Math.random()* 12);
+    let randomDigit = Math.floor(Math.random()* 11);
     return elementArray[randomDigit];
 }
 
